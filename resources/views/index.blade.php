36c54a263aa53984c360518578b03174681bcd4a
Rafael Gomes <maciel.rafael1@gmail.com>
2023-08-23 10:20:59 -0400

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
+   <div>
+      @forelse ($tasks as $task)
+         <div>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+         </div>
+      @empty
+         <div>There are no tasks</div>
+      @endforelse
+   </div>
+@endsection
